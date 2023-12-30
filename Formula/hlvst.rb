069f1718c345fbfd395409398d5af373710865e3
yigegongjiang <one.gongjiang@gmail.com>
2023-12-30 23:31:59 +0800

    bot fix

--- Formula/hlvst.rb
+++ Formula/hlvst.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Hlvst < Formula
   desc "Sentence for text, document content. Text correct for chinese."
   homepage "https://github.com/yigegongjiang/HLVSentence"
@@ -8,22 +5,11 @@ class Hlvst < Formula
   sha256 "7e5970ec75925f22b4c9b2853d2b5f59b6b52bc8fc9e1b77b8ab029356a36252"
   license "MIT License. Copyright (c) 2023 hailv"
 
-  # depends_on "cmake" => :build
-
   def install
     bin.install "hlvst"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test hlvst`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
