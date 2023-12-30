57877144d3db49d88f9bcb406692a51184da4f6e
yigegongjiang <one.gongjiang@gmail.com>
2023-12-30 23:45:52 +0800

    bot fix4

--- Formula/hlvst.rb
+++ Formula/hlvst.rb
@@ -1,5 +1,5 @@
 class Hlvst < Formula
-  desc "Sentence for text, document content. Text correct for chinese"
+  desc "Sentence for text, document. Text correct for chinese"
   homepage "https://github.com/yigegongjiang/HLVSentence"
   url "https://github.com/yigegongjiang/HLVSentence/releases/download/v1.0.2/hlvst.zip"
   sha256 "7e5970ec75925f22b4c9b2853d2b5f59b6b52bc8fc9e1b77b8ab029356a36252"
