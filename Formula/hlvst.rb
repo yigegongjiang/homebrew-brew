3e75a278ea39071c3f7586b6ed1456b3fda685a5
yigegongjiang <one.gongjiang@gmail.com>
2023-12-30 23:39:28 +0800

    bot fix3

--- Formula/hlvst.rb
+++ Formula/hlvst.rb
@@ -3,7 +3,7 @@ class Hlvst < Formula
   homepage "https://github.com/yigegongjiang/HLVSentence"
   url "https://github.com/yigegongjiang/HLVSentence/releases/download/v1.0.2/hlvst.zip"
   sha256 "7e5970ec75925f22b4c9b2853d2b5f59b6b52bc8fc9e1b77b8ab029356a36252"
-  license "MIT License. Copyright (c) 2023 hailv"
+  license "MIT"
 
   def install
     bin.install "hlvst"
